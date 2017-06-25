c799a2453e4f56e99a54754d7c6449364fa0a24b
Yigit Boyar <yboyar@google.com>
2017-06-25 13:28:41 -0700

    add copyright header to the new file

--- GithubBrowserSample/app/src/androidTest/java/com/android/example/github/util/AutoClearedValueTest.java
+++ GithubBrowserSample/app/src/androidTest/java/com/android/example/github/util/AutoClearedValueTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.android.example.github.util;
 
 import com.android.example.github.testing.SingleFragmentActivity;
@@ -8,16 +23,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import android.support.test.InstrumentationRegistry;
-import android.support.test.annotation.UiThreadTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.v4.app.Fragment;
 
-import java.util.concurrent.CountDownLatch;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
 
 @RunWith(AndroidJUnit4.class)
