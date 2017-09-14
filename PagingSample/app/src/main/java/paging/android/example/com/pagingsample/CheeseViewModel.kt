456d05289294ec32592ee0b8278cb95c7acbc32a
Chris Craik <chriscraik@gmail.com>
2017-09-14 13:13:56 -0700

    Add PagingSample (#153)
    This CL adds a new sample for the new paging library.

---
+++ PagingSample/app/src/main/java/paging/android/example/com/pagingsample/CheeseViewModel.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package paging.android.example.com.pagingsample
+
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+import android.arch.paging.PagedList
+
+/**
+ * A simple ViewModel that provides a paged list of delicious Cheeses.
+ */
+class CheeseViewModel(app: Application) : AndroidViewModel(app) {
+    val dao = CheeseDb.get(app).cheeseDao()
+
+    companion object {
+        /**
+         * A good page size is a value that fills at least a screen worth of content on a large
+         * device so the User is unlikely to see a null item.
+         * You can play with this constant to observe the paging behavior.
+         * <p>
+         * It's possible to vary this with list device size, but often unnecessary, unless a user
+         * scrolling on a large device is expected to scroll through items more quickly than a small
+         * device, such as when the large device uses a grid layout of items.
+         */
+        private const val PAGE_SIZE = 30
+
+        /**
+         * If placeholders are enabled, PagedList will report the full size but some items might
+         * be null in onBind method (PagedListAdapter triggers a rebind when data is loaded).
+         * <p>
+         * If placeholders are disabled, onBind will never receive null but as more pages are
+         * loaded, the scrollbars will jitter as new pages are loaded. You should probably disable
+         * scrollbars if you disable placeholders.
+         */
+        private const val ENABLE_PLACEHOLDERS = true
+    }
+
+    val allCheeses = dao.allCheesesByName().create(0,
+            PagedList.Config.Builder()
+                    .setPageSize(PAGE_SIZE)
+                    .setEnablePlaceholders(ENABLE_PLACEHOLDERS)
+                    .build())!!
+
+    fun insert(text: CharSequence) = ioThread {
+        dao.insert(Cheese(id = 0, name = text.toString()))
+    }
+
+    fun remove(cheese: Cheese) = ioThread {
+        dao.delete(cheese)
+    }
+}
