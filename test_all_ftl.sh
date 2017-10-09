2a7e583816e53c801b7d494fc076260f34ac27fb
Jose Alcerreca <jalc@google.com>
2017-10-09 19:10:37 +0200

    Adds unit tests and fixes paths

--- test_all_ftl.sh
+++ test_all_ftl.sh
@@ -12,7 +12,8 @@ for line in $(find . -name 'gradlew'); do
    pushd $p > /dev/null  # Silent pushd
    ./gradlew :app:assembleDebug -PdisablePreDex | sed "s@^@$p @"  # Prefix every line with directory
    ./gradlew :app:assembleAndroidTest -PdisablePreDex | sed "s@^@$p @"  # Prefix every line with directory
-   echo "y" | sudo /opt/google-cloud-sdk/bin/gcloud firebase test android run --app app/build/outputs/apk/app-debug.apk --test app/build/outputs/apk/app-debug-androidTest.apk -d Nexus5X -v 21,25 -l fr --results-bucket cloud-test-android-devrel-ci
+   ./gradlew test | sed "s@^@$p @"  # Prefix every line with directory
+   echo "y" | sudo /opt/google-cloud-sdk/bin/gcloud firebase test android run --app app/build/outputs/apk/debug/app-debug.apk --test app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk -d Nexus5X -v 21,25 -l fr --results-bucket cloud-test-android-devrel-ci
    code=${PIPESTATUS[0]}
    if [ "$code" -ne "0" ]; then
        exit $code
