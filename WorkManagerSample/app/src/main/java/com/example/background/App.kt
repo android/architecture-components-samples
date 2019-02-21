21c32ed84e4efaab30eacfc7ff083ecfe0a17ca0
Rahul Ravikumar <rahulrav@google.com>
2019-02-21 11:16:38 -0800

    Update sample to use WorkManager 1.0.0-rc02
    Change-Id: Iff4061932f464045c229772b389ff34e81a305a0

---
+++ WorkManagerSample/app/src/main/java/com/example/background/App.kt
@@ -0,0 +1,20 @@
+package com.example.background
+
+import android.app.Application
+import android.util.Log
+import androidx.work.Configuration
+import androidx.work.WorkManager
+
+/**
+ * The [Application]. Responsible for initializing [WorkManager] in [Log.VERBOSE] mode.
+ */
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        val configuration = Configuration.Builder()
+                .setMinimumLoggingLevel(Log.VERBOSE)
+                .build()
+
+        WorkManager.initialize(this, configuration)
+    }
+}
