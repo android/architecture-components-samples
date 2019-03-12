9c3030ff19e963d9f682271bce5fd27c72f81b13
Dustin Lam <dustinlam@google.com>
2019-03-12 14:26:50 -0700

    Cleanup WorkManager sample

--- WorkManagerSample/benchmark/src/androidTest/java/com/example/benchmark/WorkerBenchmark.kt
+++ WorkManagerSample/benchmark/src/androidTest/java/com/example/benchmark/WorkerBenchmark.kt
@@ -18,7 +18,6 @@ import com.example.background.workers.BlurEffectFilterWorker
 import com.example.background.workers.GrayScaleFilterWorker
 import com.example.background.workers.WaterColorFilterWorker
 import kotlinx.coroutines.runBlocking
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -31,20 +30,13 @@ class WorkerBenchmark {
         private const val JETPACK = "${BaseFilterWorker.ASSET_PREFIX}images/jetpack.png"
     }
 
-    private lateinit var mContext: Context
-    private lateinit var mTargetContext: Context
+    private val context = InstrumentationRegistry.getTargetContext()
 
     @get:Rule
     val benchmarkRule = BenchmarkRule()
 
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
-
-    @Before
-    fun setUp() {
-        mContext = InstrumentationRegistry.getContext()
-        mTargetContext = InstrumentationRegistry.getTargetContext()
-    }
 
     @Test
     fun testBlurEffectFilterWorker() {
@@ -54,7 +46,7 @@ class WorkerBenchmark {
         }
 
         benchmarkRule.measure {
-            val inputStream = inputStreamFor(mContext, JETPACK)
+            val inputStream = inputStreamFor(context, JETPACK)
             worker.applyFilter(BitmapFactory.decodeStream(inputStream))
             inputStream?.close()
         }
@@ -68,7 +60,7 @@ class WorkerBenchmark {
         }
 
         benchmarkRule.measure {
-            val inputStream = inputStreamFor(mContext, JETPACK)
+            val inputStream = inputStreamFor(context, JETPACK)
             worker.applyFilter(BitmapFactory.decodeStream(inputStream))
             inputStream?.close()
         }
@@ -82,12 +74,21 @@ class WorkerBenchmark {
         }
 
         benchmarkRule.measure {
-            val inputStream = inputStreamFor(mContext, JETPACK)
+            val inputStream = inputStreamFor(context, JETPACK)
             worker.applyFilter(BitmapFactory.decodeStream(inputStream))
             inputStream?.close()
         }
     }
 
+    /**
+     * Helper method to instantiate a worker and get a reference to it.
+     *
+     * You must use a WorkerFactory set in a custom Configuration to create an instance of
+     * WorkerParams necessary for constructing a Worker. Worker instantiation through WorkManager is
+     * triggered through enqueueing a WorkRequest that depends on the required Worker.
+     *
+     * @return reference to ListenableWorker instantiated from the provided factory block
+     */
     private fun <T : ListenableWorker> createWorker(
             createWorkerBlock: (appContext: Context, workerClassName: String,
                                 workerParameters: WorkerParameters) -> T
@@ -107,7 +108,7 @@ class WorkerBenchmark {
                 .setMinimumLoggingLevel(Log.DEBUG)
                 .build()
 
-        WorkManagerTestInitHelper.initializeTestWorkManager(mTargetContext, configuration)
+        WorkManagerTestInitHelper.initializeTestWorkManager(context, configuration)
         val workManager = WorkManager.getInstance()
 
         // Enqueue a WorkRequest in a blocking way to guarantee WorkManager has instantiated the worker.
