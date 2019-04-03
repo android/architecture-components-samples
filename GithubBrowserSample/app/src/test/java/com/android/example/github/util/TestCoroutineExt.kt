1ae679cbac5be043c2d244bd0cbf10f103a56af8
Yigit Boyar <yboyar@google.com>
2019-04-03 10:37:55 -0700

    First shot at creating a network bound resource w/ coroutines

---
+++ GithubBrowserSample/app/src/test/java/com/android/example/github/util/TestCoroutineExt.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.example.github.util
+
+import kotlinx.coroutines.ObsoleteCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineContext
+
+// Utility class to observe TestCoroutineContext internals until the new TestCoroutineContext
+// APIs are available
+@ObsoleteCoroutinesApi
+fun TestCoroutineContext.isIdle(): Boolean {
+    val queueField = this::class.java
+        .getDeclaredField("queue")
+    queueField.isAccessible = true
+    val queue = queueField.get(this)
+    val peekMethod = queue::class.java
+        .getDeclaredMethod("peek")
+    val nextTask = peekMethod.invoke(queue) ?: return true
+    val timeField = nextTask::class.java.getDeclaredField("time")
+    timeField.isAccessible = true
+    val time = timeField.getLong(nextTask)
+    return time > now()
+}
