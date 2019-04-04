bb589adb106ba8f6dbcf19b7b36be4b9b9dffcfd
Yigit Boyar <yboyar@google.com>
2019-04-04 16:05:50 -0700

    fix user dao test to call suspend function

--- GithubBrowserSample/app/src/androidTest/java/com/android/example/github/db/UserDaoTest.kt
+++ GithubBrowserSample/app/src/androidTest/java/com/android/example/github/db/UserDaoTest.kt
@@ -20,6 +20,7 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.runner.AndroidJUnit4
 import com.android.example.github.util.LiveDataTestUtil.getValue
 import com.android.example.github.util.TestUtil
+import kotlinx.coroutines.runBlocking
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Rule
@@ -34,16 +35,18 @@ class UserDaoTest : DbTest() {
 
     @Test
     fun insertAndLoad() {
-        val user = TestUtil.createUser("foo")
-        db.userDao().insert(user)
+        runBlocking {
+            val user = TestUtil.createUser("foo")
+            db.userDao().insert(user)
 
-        val loaded = getValue(db.userDao().findByLogin(user.login))
-        assertThat(loaded.login, `is`("foo"))
+            val loaded = getValue(db.userDao().findByLogin(user.login))
+            assertThat(loaded.login, `is`("foo"))
 
-        val replacement = TestUtil.createUser("foo2")
-        db.userDao().insert(replacement)
+            val replacement = TestUtil.createUser("foo2")
+            db.userDao().insert(replacement)
 
-        val loadedReplacement = getValue(db.userDao().findByLogin(replacement.login))
-        assertThat(loadedReplacement.login, `is`("foo2"))
+            val loadedReplacement = getValue(db.userDao().findByLogin(replacement.login))
+            assertThat(loadedReplacement.login, `is`("foo2"))
+        }
     }
 }
