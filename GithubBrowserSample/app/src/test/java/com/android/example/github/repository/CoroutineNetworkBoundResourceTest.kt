2c518f6b8c20965d22c158ebc12a475cd355bc9c
Yigit Boyar <yboyar@google.com>
2019-04-27 23:13:56 -0700

    remove duplicated test

--- GithubBrowserSample/app/src/test/java/com/android/example/github/repository/CoroutineNetworkBoundResourceTest.kt
+++ GithubBrowserSample/app/src/test/java/com/android/example/github/repository/CoroutineNetworkBoundResourceTest.kt
@@ -192,24 +192,6 @@ class CoroutineNetworkBoundResourceTest : CoroutineTestBase() {
         }
     }
 
-    @Test
-    fun removeObserverWhileRunning() {
-        val dbData = MutableLiveData<Foo>()
-        val ld = networkBoundResource<Foo, Foo>(
-            saveCallResult = {throw AssertionError("should not call")},
-            fetch = {throw AssertionError("should not call")},
-            loadFromDb = { dbData }
-        )
-        ld.addObserver().apply {
-            assertItems(Resource.loading(null))
-            assertThat(dbData.hasObservers(), `is`(true))
-            unsubscribe()
-            advanceTimeBy(10_000)
-            assertThat(dbData.hasObservers(), `is`(false))
-            assertItems(Resource.loading(null))
-        }
-    }
-
     private fun advanceTimeBy(time: Long) {
         testMainContext.advanceTimeBy(time)
         testBackgroundContext.advanceTimeBy(time)
