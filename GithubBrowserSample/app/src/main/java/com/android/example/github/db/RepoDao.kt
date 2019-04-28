30a21b3ba7d49dc2551e03107640bd27345a153a
Yigit Boyar <yboyar@google.com>
2019-04-28 00:08:49 -0700

    WIP move repo to coroutines

--- GithubBrowserSample/app/src/main/java/com/android/example/github/db/RepoDao.kt
+++ GithubBrowserSample/app/src/main/java/com/android/example/github/db/RepoDao.kt
@@ -37,7 +37,7 @@ import java.util.Collections
 abstract class RepoDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    abstract fun insert(vararg repos: Repo)
+    abstract suspend fun insert(repos: Repo)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     abstract fun insertContributors(contributors: List<Contributor>)
