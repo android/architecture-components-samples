4dc219880f9fde4ffc067df35dac6bae9acde82a
Dustin Lam <dustinlam@google.com>
2019-04-30 13:20:22 -0700

    Update workmanager samples to use new test api

--- WorkManagerSample/app/src/androidTest/java/com/example/background/ImageOperationsTest.kt
+++ WorkManagerSample/app/src/androidTest/java/com/example/background/ImageOperationsTest.kt
@@ -27,10 +27,10 @@ import android.util.Log
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
-import androidx.test.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SdkSuppress
 import androidx.test.filters.SmallTest
-import androidx.test.runner.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import androidx.work.Configuration
 import androidx.work.WorkManager
 import androidx.work.testing.SynchronousExecutor
@@ -76,8 +76,8 @@ class ImageOperationsTest {
 
     @Before
     fun setUp() {
-        mContext = InstrumentationRegistry.getContext()
-        mTargetContext = InstrumentationRegistry.getTargetContext()
+        mContext = InstrumentationRegistry.getInstrumentation().context
+        mTargetContext = InstrumentationRegistry.getInstrumentation().targetContext
         mLifeCycleOwner = TestLifeCycleOwner()
         mConfiguration = Configuration.Builder()
                 .setExecutor(SynchronousExecutor())
