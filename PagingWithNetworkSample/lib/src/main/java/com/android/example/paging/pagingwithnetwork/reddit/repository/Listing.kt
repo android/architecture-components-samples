6a78a194b923c4d6876b7a91bce07cb25ff4aeaf
Dustin Lam <dustinlam@google.com>
2019-07-12 13:33:56 -0700

    Update to new paging api

--- PagingWithNetworkSample/lib/src/main/java/com/android/example/paging/pagingwithnetwork/reddit/repository/Listing.kt
+++ PagingWithNetworkSample/lib/src/main/java/com/android/example/paging/pagingwithnetwork/reddit/repository/Listing.kt
@@ -22,7 +22,7 @@ import androidx.paging.PagedList
 /**
  * Data class that is necessary for a UI to show a listing and interact w/ the rest of the system
  */
-data class Listing<T>(
+data class Listing<T : Any>(
         // the LiveData of paged lists for the UI to observe
         val pagedList: LiveData<PagedList<T>>,
         // represents the network request status to show to the user
