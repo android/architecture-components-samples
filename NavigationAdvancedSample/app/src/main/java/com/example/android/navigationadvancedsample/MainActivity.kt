97888fd15219f793cfb83a9c1d39c14c34f1ee04
Eugeniu Tufar <pacific.trac@gmail.com>
2020-01-13 16:45:22 +0200

    Update MainActivity.kt
    Now you update currenctNavController on each navigation.

--- NavigationAdvancedSample/app/src/main/java/com/example/android/navigationadvancedsample/MainActivity.kt
+++ NavigationAdvancedSample/app/src/main/java/com/example/android/navigationadvancedsample/MainActivity.kt
@@ -65,9 +65,9 @@ class MainActivity : AppCompatActivity() {
 
         // Whenever the selected controller changes, setup the action bar.
         controller.observe(this, Observer { navController ->
-            setupActionBarWithNavController(navController)
+            setupActionBarWithNavController(navController)                               
+            currentNavController = controller
         })
-        currentNavController = controller
     }
 
     override fun onSupportNavigateUp(): Boolean {
