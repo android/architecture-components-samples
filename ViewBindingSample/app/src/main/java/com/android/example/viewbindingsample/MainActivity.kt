ae4474c6baf12897feb87dd86d13dbc64ccc3231
Jose Alcerreca <jalc@google.com>
2020-01-22 12:23:43 +0100

    Initial commit ViewBindingSample

---
+++ ViewBindingSample/app/src/main/java/com/android/example/viewbindingsample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.android.example.viewbindingsample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
