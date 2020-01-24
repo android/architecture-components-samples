0edbc8f467729bb922db0d5be35d38c0ebc14711
Jose Alcerreca <jalc@google.com>
2020-01-24 11:20:19 +0100

    Adds bind API to View Binding sample

--- ViewBindingSample/app/src/androidTest/java/com/android/example/viewbindingsample/SimpleUiTest.kt
+++ ViewBindingSample/app/src/androidTest/java/com/android/example/viewbindingsample/SimpleUiTest.kt
@@ -38,6 +38,7 @@ class SimpleUiTest {
     @Test
     fun activityLaunches() {
         onView(withText(R.string.hello_from_vb_activity)).check(matches(isDisplayed()))
-        onView(withText(R.string.hello_from_vb_fragment)).check(matches(isDisplayed()))
+        onView(withText(R.string.hello_from_vb_bindfragment)).check(matches(isDisplayed()))
+        onView(withText(R.string.hello_from_vb_inflatefragment)).check(matches(isDisplayed()))
     }
 }
--- ViewBindingSample/app/src/main/java/com/android/example/viewbindingsample/InflateFragment.kt
+++ ViewBindingSample/app/src/main/java/com/android/example/viewbindingsample/InflateFragment.kt
@@ -21,10 +21,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.android.example.viewbindingsample.R.string
 import com.android.example.viewbindingsample.databinding.FragmentBlankBinding
 
-class BlankFragment : Fragment() {
+/**
+ * View Binding example with a fragment that uses the traditional constructor and [onCreateView] for
+ * inflation.
+ */
+class InflateFragment : Fragment() {
 
     // Scoped to the lifecycle of the fragment's view (between onCreateView and onDestroyView)
     private var fragmentBlankBinding: FragmentBlankBinding? = null
@@ -36,7 +39,7 @@ class BlankFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding = FragmentBlankBinding.inflate(inflater, container, false)
         fragmentBlankBinding = binding
-        binding.textViewFragment.text = getString(string.hello_from_vb_fragment)
+        binding.textViewFragment.text = getString(R.string.hello_from_vb_inflatefragment)
         return binding.root
     }
 
