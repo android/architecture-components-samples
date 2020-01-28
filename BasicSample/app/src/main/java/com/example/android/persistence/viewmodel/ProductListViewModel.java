ea66fae12134929cd1797cdfd8af5bb3bfb716d7
Ian Lake <ian.hannibal.lake@gmail.com>
2020-01-27 22:57:00 -0800

    Avoid multiple concurrent observe() calls with Transformations.switchMap
    Instead of starting a new `observe` call every time the query button is pressed, we should just update our current source of data by using `Transformations.switchMap`. We store the user's query in a `SavedStateHandle` to ensure that we handle process death and recreation properly (and to take advantage of the `getLiveData()` method to drive our `switchMap`.
    
    Test: tests still pass
    
    Fixes #759

--- BasicSample/app/src/main/java/com/example/android/persistence/viewmodel/ProductListViewModel.java
+++ BasicSample/app/src/main/java/com/example/android/persistence/viewmodel/ProductListViewModel.java
@@ -17,43 +17,59 @@
 package com.example.android.persistence.viewmodel;
 
 import android.app.Application;
-import androidx.lifecycle.AndroidViewModel;
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MediatorLiveData;
+import android.text.TextUtils;
+
 import com.example.android.persistence.BasicApp;
 import com.example.android.persistence.DataRepository;
 import com.example.android.persistence.db.entity.ProductEntity;
+
 import java.util.List;
 
+import androidx.annotation.NonNull;
+import androidx.arch.core.util.Function;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.SavedStateHandle;
+import androidx.lifecycle.Transformations;
+
 public class ProductListViewModel extends AndroidViewModel {
+    private static final String QUERY_KEY = "QUERY";
 
+    private final SavedStateHandle mSavedStateHandler;
     private final DataRepository mRepository;
+    private final LiveData<List<ProductEntity>> mProducts;
 
-    // MediatorLiveData can observe other LiveData objects and react on their emissions.
-    private final MediatorLiveData<List<ProductEntity>> mObservableProducts;
-
-    public ProductListViewModel(Application application) {
+    public ProductListViewModel(@NonNull Application application,
+            @NonNull SavedStateHandle savedStateHandle) {
         super(application);
-
-        mObservableProducts = new MediatorLiveData<>();
-        // set by default null, until we get data from the database.
-        mObservableProducts.setValue(null);
+        mSavedStateHandler = savedStateHandle;
 
         mRepository = ((BasicApp) application).getRepository();
-        LiveData<List<ProductEntity>> products = mRepository.getProducts();
 
-        // observe the changes of the products from the database and forward them
-        mObservableProducts.addSource(products, mObservableProducts::setValue);
+        // Use the savedStateHandle.getLiveData() as the input to switchMap,
+        // allowing us to recalculate what LiveData to get from the DataRepository
+        // based on what query the user has entered
+        mProducts = Transformations.switchMap(
+                savedStateHandle.getLiveData("QUERY", null),
+                (Function<CharSequence, LiveData<List<ProductEntity>>>) query -> {
+                    if (TextUtils.isEmpty(query)) {
+                        return mRepository.getProducts();
+                    }
+                    return mRepository.searchProducts("*" + query + "*");
+                });
+    }
+
+    public void setQuery(CharSequence query) {
+        // Save the user's query into the SavedStateHandle.
+        // This ensures that we retain the value across process death
+        // and is used as the input into the Transformations.switchMap above
+        mSavedStateHandler.set(QUERY_KEY, query);
     }
 
     /**
      * Expose the LiveData Products query so the UI can observe it.
      */
     public LiveData<List<ProductEntity>> getProducts() {
-        return mObservableProducts;
-    }
-
-    public LiveData<List<ProductEntity>> searchProducts(String query) {
-        return mRepository.searchProducts(query);
+        return mProducts;
     }
 }
