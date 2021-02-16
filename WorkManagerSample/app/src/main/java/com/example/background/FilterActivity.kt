c5f478f11084bd3d36b360c8cce1f041a0a7a66e
Ben Weiss <benweiss@google.com>
2021-02-16 15:12:12 +0000

    Refactor FilterActivity

--- WorkManagerSample/app/src/main/java/com/example/background/FilterActivity.kt
+++ WorkManagerSample/app/src/main/java/com/example/background/FilterActivity.kt
@@ -23,34 +23,38 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
+import androidx.work.WorkInfo
 import com.bumptech.glide.Glide
-import com.example.background.databinding.ActivityProcessingBinding
+import com.example.background.databinding.ActivityFilterBinding
 
 /** The [android.app.Activity] where the user picks filters to be applied on an image. */
 class FilterActivity : AppCompatActivity() {
 
     private val viewModel: FilterViewModel by viewModels { FilterViewModelFactory(application) }
-    private var imageUri: Uri? = null
     private var outputImageUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityProcessingBinding.inflate(layoutInflater).apply {
+
+        ActivityFilterBinding.inflate(layoutInflater).run {
             setContentView(root)
+            bindViews(this)
+            // Check to see if we have output.
+            viewModel.outputStatus.observe(this@FilterActivity) { infoList ->
+                onStateChange(infoList, this)
+            }
         }
+    }
 
-        // Don't enable upload to Imgur, unless the developer specifies their own clientId.
-        val enableUpload = Constants.IMGUR_CLIENT_ID.isNotEmpty()
-        binding.upload.isEnabled = enableUpload
-
-        val imageUriExtra = intent.getStringExtra(Constants.KEY_IMAGE_URI)
-        if (!imageUriExtra.isNullOrEmpty()) {
-            imageUri = Uri.parse(imageUriExtra)
-            Glide.with(this).load(imageUri).into(binding.imageView)
-        }
-
+    private fun bindViews(binding: ActivityFilterBinding) {
         with(binding) {
+            val imageUri: Uri = Uri.parse(intent.getStringExtra(Constants.KEY_IMAGE_URI))
+            Glide.with(this@FilterActivity).load(imageUri).into(imageView)
+
+            // Disable upload to Imgur, unless the developer specifies their own clientId.
+            val uploadEnabled = Constants.IMGUR_CLIENT_ID.isNotEmpty()
+            upload.isEnabled = uploadEnabled
+
             apply.setOnClickListener {
                 val applyWaterColor = filterWatercolor.isChecked
                 val applyGrayScale = filterGrayscale.isChecked
@@ -59,57 +63,52 @@ class FilterActivity : AppCompatActivity() {
                 val upload = upload.isChecked
 
                 val imageOperations = ImageOperations(
-                    applicationContext, imageUri!!,
+                    applicationContext, imageUri,
                     applyWaterColor, applyGrayScale, applyBlur,
                     save, upload
                 )
 
                 viewModel.apply(imageOperations)
             }
-        }
 
-        binding.output.setOnClickListener {
-            if (outputImageUri != null) {
-                val viewOutput = Intent(Intent.ACTION_VIEW, outputImageUri)
-                if (viewOutput.resolveActivity(packageManager) != null) {
-                    startActivity(viewOutput)
+            output.setOnClickListener {
+                if (outputImageUri != null) {
+                    val viewOutput = Intent(Intent.ACTION_VIEW, outputImageUri)
+                    if (viewOutput.resolveActivity(packageManager) != null) {
+                        startActivity(viewOutput)
+                    }
                 }
             }
+            cancel.setOnClickListener { viewModel.cancel() }
         }
+    }
 
-        binding.cancel.setOnClickListener { viewModel.cancel() }
+    private fun onStateChange(infoList: List<WorkInfo>, binding: ActivityFilterBinding) {
+        if (infoList.isEmpty()) {
+            return
+        }
+        // We only care about the one output status.
+        // Every continuation has only one worker tagged TAG_OUTPUT
+        val info = infoList[0]
+        val finished = info.state.isFinished
 
-        // Check to see if we have output.
-        viewModel.outputStatus.observe(this, Observer { infoList ->
-            if (infoList.isNullOrEmpty()) {
-                return@Observer
-            }
-
-            // We only care about the one output status.
-            // Every continuation has only one worker tagged TAG_OUTPUT
-            val info = infoList[0]
-            val finished = info.state.isFinished
+        with(binding) {
             if (!finished) {
-                with(binding) {
-                    progressBar.visibility = View.VISIBLE
-                    cancel.visibility = View.VISIBLE
-                    apply.visibility = View.GONE
-                    output.visibility = View.GONE
-                }
+                progressBar.visibility = View.VISIBLE
+                cancel.visibility = View.VISIBLE
+                apply.visibility = View.GONE
+                output.visibility = View.GONE
             } else {
-                with(binding) {
-                    progressBar.visibility = View.GONE
-                    cancel.visibility = View.GONE
-                    apply.visibility = View.VISIBLE
-                }
-
-                val outputData = info.outputData
-                outputData.getString(Constants.KEY_IMAGE_URI)?.let {
-                    outputImageUri = Uri.parse(it)
-                    binding.output.visibility = View.VISIBLE
-                }
+                progressBar.visibility = View.GONE
+                cancel.visibility = View.GONE
+                apply.visibility = View.VISIBLE
             }
-        })
+        }
+        val outputData = info.outputData
+        outputData.getString(Constants.KEY_IMAGE_URI)?.let {
+            outputImageUri = Uri.parse(it)
+            binding.output.visibility = View.VISIBLE
+        }
     }
 
     companion object {
