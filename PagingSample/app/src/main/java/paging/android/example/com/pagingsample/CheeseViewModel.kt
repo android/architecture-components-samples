5e86e72c4097a9c86b339cfa7150cc424134c00c
claraf3 <clarafok3@gmail.com>
2021-05-21 13:18:24 -0700

    change fix to use equals function

--- PagingSample/app/src/main/java/paging/android/example/com/pagingsample/CheeseViewModel.kt
+++ PagingSample/app/src/main/java/paging/android/example/com/pagingsample/CheeseViewModel.kt
@@ -78,8 +78,7 @@ class CheeseViewModel(private val dao: CheeseDao) : ViewModel() {
                     } else if (before == null) {
                         // Header
                         CheeseListItem.Separator(after.name.first())
-                    } else if (before.name.first().toLowerCase() !=
-                        after.name.first().toLowerCase()) {
+                    } else if (!before.name.first().equals(after.name.first(), ignoreCase = true)){
                         // Between two items that start with different letters.
                         CheeseListItem.Separator(after.name.first())
                     } else {
